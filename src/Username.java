183526346cbc2e297ee56bbe35578faf97be1d50
Shea salloum <sheamon15@gmail.com>
2017-09-05 16:34:25 -0400

    initial commmit

---
+++ src/Username.java
@@ -0,0 +1,23 @@
+/**
+ * Author@ Shea Salloum
+ * License@GNU
+ * purpose: Display a enter username and enter password screen then welcome the user
+ */
+
+import java.util.Scanner;
+public class Username {
+    public static void main(String[] args){
+            String name;
+            Scanner keyboard = new Scanner(System.in);
+                System.out.println("Enter your Username");
+                name = keyboard.nextLine();
+                System.out.println("Enter Your Password");
+                String password;
+                password = keyboard.nextLine();
+
+
+                //output to display
+                System.out.print("Hello, " + name);
+                System.out.print( " Welcome to CSC200 class! and Your password is " + password);
+    }
+}
